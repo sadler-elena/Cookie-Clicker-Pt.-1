6f5c35d019fed4e86792a3061600e6a9b7acf856
Elena <elena_sadler@yahoo.com>
2020-12-21 13:09:44 -0600

    Initial commit

---
+++ Shared/Cookie_Clicker_Pt__1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_Clicker_Pt__1App.swift
+//  Shared
+//
+//  Created by Elena Sadler on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_Clicker_Pt__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
